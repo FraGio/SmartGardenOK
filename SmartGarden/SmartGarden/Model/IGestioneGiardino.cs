d870825d742e9f9071970dee25c12bd1d7ea9080
Andrea Sabbioni <andrea.sabbioni3@studio.unibo.it>
2015-10-21 12:45:59 +0200

    asd

---
+++ SmartGarden/SmartGarden/Model/IGestioneGiardino.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGarden.Model
+{
+    interface IGestioneGiardino
+    {
+        IGiardino Giardino { get; set; }
+        string Luogo { get; set; }
+        event EventHandler Changed;
+        DateTime OraInizioInnaffiatura { get; }
+        TimeSpan Intervallo { get; }
+        void SetTimer(DateTime date, TimeSpan intervallo);
+  
+    }
+}
