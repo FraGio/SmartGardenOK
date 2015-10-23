8cf1a5725dc2cb6c1eca0c3ccc14e71e8d4688e8
FraGio <giova6@hotmail.it>
2015-10-23 13:06:17 +0200

    cas

---
+++ SmartGarden/SmartGarden/View/NotificheView.cs
@@ -0,0 +1,34 @@
+﻿using SmartGarden.Model;
+using System;
+
+namespace SmartGarden.View
+{
+    partial class NotificheView : MainView
+    {
+        private IMessagePump _messagePump;
+
+        public NotificheView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            _messagePump = MessagePump.GetMessagePump();
+            _messagePump.newMessage += RefreshNotifiche;
+        }
+
+        private void RefreshNotifiche(object sender, EventArgs e)
+        {
+            _notificheTextBox.Clear();
+            _messagePump.GetAllMEssages();
+        }
+
+        private void _pulisciButton_Click(object sender, EventArgs e)
+        {
+            _messagePump.DeleteMessages();
+            _notificheTextBox.Clear();
+        }
+    }
+}
