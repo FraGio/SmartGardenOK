fcfb10f5f1a876a79382909a1ca1e3b7d3da9f60
FraGio <giova6@hotmail.it>
2015-10-23 10:26:14 +0200

    fix grafica

--- SmartGarden/SmartGarden/View/NuovaPiantaView.Designer.cs
+++ SmartGarden/SmartGarden/View/NuovaPiantaView.Designer.cs
@@ -41,12 +41,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this._inizioDatePicker = new System.Windows.Forms.DateTimePicker();
             this._fineDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // _aggiungiButton
             // 
             this._aggiungiButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this._aggiungiButton.Location = new System.Drawing.Point(178, 263);
+            this._aggiungiButton.Location = new System.Drawing.Point(176, 286);
             this._aggiungiButton.Name = "_aggiungiButton";
             this._aggiungiButton.Size = new System.Drawing.Size(75, 23);
             this._aggiungiButton.TabIndex = 13;
@@ -58,7 +60,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(130, 91);
+            this.label3.Location = new System.Drawing.Point(164, 91);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 15);
             this.label3.TabIndex = 11;
@@ -66,7 +68,7 @@
             // 
             // _nomeBotanicoTextBox
             // 
-            this._nomeBotanicoTextBox.Location = new System.Drawing.Point(178, 48);
+            this._nomeBotanicoTextBox.Location = new System.Drawing.Point(212, 48);
             this._nomeBotanicoTextBox.Name = "_nomeBotanicoTextBox";
             this._nomeBotanicoTextBox.Size = new System.Drawing.Size(129, 20);
             this._nomeBotanicoTextBox.TabIndex = 10;
@@ -75,7 +77,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(67, 50);
+            this.label2.Location = new System.Drawing.Point(101, 50);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 15);
             this.label2.TabIndex = 9;
@@ -83,7 +85,7 @@
             // 
             // _nomeComuneTextBox
             // 
-            this._nomeComuneTextBox.Location = new System.Drawing.Point(178, 13);
+            this._nomeComuneTextBox.Location = new System.Drawing.Point(212, 13);
             this._nomeComuneTextBox.Name = "_nomeComuneTextBox";
             this._nomeComuneTextBox.Size = new System.Drawing.Size(129, 20);
             this._nomeComuneTextBox.TabIndex = 8;
@@ -92,7 +94,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(81, 15);
+            this.label1.Location = new System.Drawing.Point(115, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 15);
             this.label1.TabIndex = 7;
@@ -100,14 +102,14 @@
             // 
             // _areaTextBox
             // 
-            this._areaTextBox.Location = new System.Drawing.Point(178, 89);
+            this._areaTextBox.Location = new System.Drawing.Point(212, 89);
             this._areaTextBox.Name = "_areaTextBox";
             this._areaTextBox.Size = new System.Drawing.Size(129, 20);
             this._areaTextBox.TabIndex = 14;
             // 
             // _fabbisognoTextBox
             // 
-            this._fabbisognoTextBox.Location = new System.Drawing.Point(178, 226);
+            this._fabbisognoTextBox.Location = new System.Drawing.Point(209, 111);
             this._fabbisognoTextBox.Name = "_fabbisognoTextBox";
             this._fabbisognoTextBox.Size = new System.Drawing.Size(129, 20);
             this._fabbisognoTextBox.TabIndex = 17;
@@ -116,7 +118,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(60, 140);
+            this.label4.Location = new System.Drawing.Point(91, 25);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(112, 15);
             this.label4.TabIndex = 18;
@@ -126,7 +128,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(74, 185);
+            this.label5.Location = new System.Drawing.Point(105, 70);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 15);
             this.label5.TabIndex = 19;
@@ -136,37 +138,47 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(88, 228);
+            this.label6.Location = new System.Drawing.Point(56, 113);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(84, 15);
+            this.label6.Size = new System.Drawing.Size(147, 15);
             this.label6.TabIndex = 20;
-            this.label6.Text = "Fabbisogno:";
+            this.label6.Text = "Quantità acqua (mm):";
             // 
             // _inizioDatePicker
             // 
-            this._inizioDatePicker.Location = new System.Drawing.Point(178, 135);
+            this._inizioDatePicker.Location = new System.Drawing.Point(209, 20);
             this._inizioDatePicker.Name = "_inizioDatePicker";
             this._inizioDatePicker.Size = new System.Drawing.Size(175, 20);
             this._inizioDatePicker.TabIndex = 21;
             // 
             // _fineDatePicker
             // 
-            this._fineDatePicker.Location = new System.Drawing.Point(178, 180);
+            this._fineDatePicker.Location = new System.Drawing.Point(209, 65);
             this._fineDatePicker.Name = "_fineDatePicker";
             this._fineDatePicker.Size = new System.Drawing.Size(175, 20);
             this._fineDatePicker.TabIndex = 22;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this._fineDatePicker);
+            this.groupBox1.Controls.Add(this._fabbisognoTextBox);
+            this.groupBox1.Controls.Add(this._inizioDatePicker);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(3, 127);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 141);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fabbisogno giornaliero";
             // 
             // NuovaPiantaView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.Controls.Add(this._fineDatePicker);
-            this.Controls.Add(this._inizioDatePicker);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this._fabbisognoTextBox);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this._areaTextBox);
             this.Controls.Add(this._aggiungiButton);
             this.Controls.Add(this.label3);
@@ -175,7 +187,9 @@
             this.Controls.Add(this._nomeComuneTextBox);
             this.Controls.Add(this.label1);
             this.Name = "NuovaPiantaView";
-            this.Size = new System.Drawing.Size(412, 305);
+            this.Size = new System.Drawing.Size(413, 321);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,5 +210,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DateTimePicker _inizioDatePicker;
         private System.Windows.Forms.DateTimePicker _fineDatePicker;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
